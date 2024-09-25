2be7405c07ebd0fe8e6d62eccc0986a794ff4a73
ENZO GONCALVES SANTOS <enzo.gsantos@senacsp.edu.br>
2024-09-25 14:32:46 -0300

    primeiro commit

---
+++ sript.js
@@ -0,0 +1,11 @@
+// Solicitar a idade do usuário
+
+let idade = prompt("Qual sua idade?")
+ 
+// verificar se a idade é maior que 18
+
+if (idade >= 18) {
+    alert("Acesso permitido! 🙂")
+    
+}else{
+    alert("Acesso negado 🤨")}
